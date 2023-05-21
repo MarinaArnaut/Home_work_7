125a65a5cd457b0c83e44031d3e96110d28e374d
Marina_Arnaut <skital87@mail.ru>
2023-05-21 19:40:23 +1000

    case 34

--- lesson 8.py
+++
@@ -1,3 +0,0 @@
-f = open('1.txt', 'r')
-stroka1 = f.readline()
-print(stroka1)
--- вкл.в лист чисел как строк.py
+++
@@ -1,5 +0,0 @@
-my_list = [15, 17, 16]
-my_list3 = list(map(lambda x: str(x)+'\n', my_list))
-print(my_list3)
-with open('1.txt', 'w') as f:
-    f.writelines(my_list3)
